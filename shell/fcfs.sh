77ea01582aa87ae04625f6b754415e451024a23c
sungness <sungness@gmail.com>
2021-05-30 18:59:28 +0800

    add list server function and check args function

--- shell/fcfs.sh
+++ shell/fcfs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 fcfs_settings_file="fcfs.settings"
-fcfs_dependency_file_server="http://www.fastken.com/"
+#fcfs_dependency_file_server="http://www.fastken.com"
+fcfs_dependency_file_server="http://localhost:8082"
+module_names=(fdir fstore fauth fuseclient)
 shell_name=$0
 shell_command=$1
 uname=$(uname)
@@ -36,17 +38,7 @@ check_install_fastos_repo() {
   fi
 }
 
-install_dependent_libs() {
-  if [ $osname = 'CentOS' ] && [ $os_major_version -eq 7 -o $os_major_version -eq 8 ]; then
-    check_install_fastos_repo
-    rpm -q fuse >/dev/null && yum remove fuse -y
-    yum install FastCFS-auth-server fastDIR-server faststore-server FastCFS-fused -y
-  else
-    echo "Unsupport OS: $uname" 1>&2
-    echo "Command setup and install can only be used for CentOS 7 or 8."
-    exit 1
-  fi
-}
+
 
 # View last N lines log of the cluster with system command tail;
 tail_log() {
@@ -73,7 +65,7 @@ print_usage() {
   echo "  fdir       fastDIR"
   echo "  fstore     faststore"
   echo "  fauth      FastCFS auth client module"
-  echo "  fsclient   FastCFS client use with fuse"
+  echo "  fuseclient   FastCFS client use with fuse"
   echo ""
   echo "Node:"
   echo "  You can specify a single cluster IP, or command will be executed on all nodes."
@@ -99,16 +91,16 @@ load_fcfs_settings() {
   else
     fcfs_settings=`sed -e 's/#.*//' -e '/^$/ d' $fcfs_settings_file`
     eval $fcfs_settings
-    if [ -z $fastcfs_version ]; then
-      echo "WARN: param fastcfs_version has no value in $fcfs_settings_file"
-    fi
-    if [ -z $fsclient_ips ]; then
-      echo "WARN: param fsclient_ips has no value in $fcfs_settings_file"
-    fi 
+    # if [ -z $fastcfs_version ]; then
+    #   echo "WARN: param fastcfs_version has no value in $fcfs_settings_file"
+    # fi
+    # if [ -z $fuseclient_ips ]; then
+    #   echo "WARN: param fuseclient_ips has no value in $fcfs_settings_file"
+    # fi 
   fi
 }
 
-# Load cluster dependent libs settings from file dependency.[FastCFS-version].settings
+# Load cluster dependent lib version settings from file dependency.[FastCFS-version].settings
 load_dependency_settings() {
   dependency_file_version=$1
   if [ -z $dependency_file_version ]; then
@@ -118,46 +110,159 @@ load_dependency_settings() {
   dependency_settings_file="dependency.$dependency_file_version.settings"
   if ! [ -f $dependency_settings_file ]; then
     # File not exist in local path, will get it from remote server match the version
-    echo "WARN: file $dependency_settings_file not exist, getting it from remote server"
+    echo "WARN: file $dependency_settings_file not exist, getting it from remote server $fcfs_dependency_file_server"
     remote_file_url="$fcfs_dependency_file_server/$dependency_settings_file"
-  else
-    fcfs_settings=`sed -e 's/#.*//' -e '/^$/ d' $fcfs_settings_file`
-    eval $fcfs_settings
-    if [ -z $fastcfs_version ]; then
-      echo "WARN: param fastcfs_version has no value in $fcfs_settings_file"
+    download_res=`curl -f -o $dependency_settings_file $remote_file_url`
+
+    if ! [ -f $dependency_settings_file ]; then
+      echo "Error: cannot download file $dependency_settings_file from $remote_file_url"
+      echo "Please make sure that remote file exists and network is accessible"
+      exit 1
     fi
-    if [ -z $fsclient_ips ]; then
-      echo "WARN: param fsclient_ips has no value in $fcfs_settings_file"
-    fi 
+  fi
+  dependency_settings=`sed -e 's/#.*//' -e '/^$/ d' $dependency_settings_file`
+  eval $dependency_settings
+  if [ -z $libfastcommon ]; then
+    echo "WARN: dependency libfastcommon has no version value in $dependency_settings_file"
+  fi
+  if [ -z $libserverframe ]; then
+    echo "WARN: dependency libserverframe has no version value in $dependency_settings_file"
+  fi
+  if [ -z $fauth ]; then
+    echo "WARN: dependency fauth has no version value in $dependency_settings_file"
+  fi
+  if [ -z $fdir ]; then
+    echo "WARN: dependency fdir has no version value in $dependency_settings_file"
+  fi
+  if [ -z $fstore ]; then
+    echo "WARN: dependency fstore has no version value in $dependency_settings_file"
   fi
 }
 
-load_fcfs_settings
+# Get module name from command args, if not specify, will handle all modules.
+check_module_param() {
+  if [ $# -gt 1 ]; then
+    module_name=$2
+    case "$module_name" in
+      'fdir')
+        fdir_need_execute=1
+      ;;
+      'fstore')
+        fstore_need_execute=1
+      ;;
+      'fauth')
+        fauth_need_execute=1
+      ;;
+      'fuseclient')
+        fuseclient_need_execute=1
+      ;;
+      *)
+        echo "Error: you specifed module name invalid:$module_name"
+        echo "Allowed module names:fdir/fstore/fauth/fuseclient"
+        exit 1
+      ;;
+    esac
+  else
+    fdir_need_execute=1
+    fstore_need_execute=1
+    fauth_need_execute=1
+    fuseclient_need_execute=1
+  fi
+}
 
+# Get node host from command args, if not specify, will handle all nodes.
+# It must specify after module name.
+check_node_param() {
+  if [ $# -gt 2 ]; then
+    node_host_need_execute=$3
+  fi
+}
+
+list_servers_in_config() {
+  list_program=$1
+  list_config_file=$2
+  cmd_exist=`which $list_program`
+  if [ -z "$cmd_exist" ]; then
+    echo "Error: program $list_program not found, you must install it first."
+    exit 1
+  else
+    if ! [ -f $list_config_file ]; then
+      echo "Error: cluster config file $list_config_file not exist"
+      exit 1
+    else
+      list_result=$($list_program $list_config_file)
+      echo "Info: get servers from $list_config_file:"
+      echo "$list_result"
+      eval $list_result
+    fi
+  fi
+}
+
+# Load cluster groups from cluster config files.
+#   fdir_group=(172.16.168.128)
+#   fstore_group_count=1
+#   fstore_group_1=(172.16.168.128)
+#   fauth_group=(172.16.168.128)
+load_cluster_groups() {
+  if [ $fdir_need_execute -eq 1 ]; then
+    list_servers_in_config fdir_list_servers conf/fdir/cluster.conf
+  fi
+  if [ $fstore_need_execute -eq 1 ]; then
+    list_servers_in_config fstore_list_servers conf/fstore/cluster.conf
+  fi
+  if [ $fauth_need_execute -eq 1 ]; then
+    list_servers_in_config fauth_list_servers conf/fauth/cluster.conf
+  fi
+}
+
+# Install libs to target nodes.
+install_dependent_libs() {
+  load_fcfs_settings
+  if [ -z $fastcfs_version ]; then
+    echo "Error: fastcfs_version in $fcfs_settings_file cannot be empty"
+    exit 1
+  fi
+  load_dependency_settings $fastcfs_version
+
+  # if [ $osname = 'CentOS' ] && [ $os_major_version -eq 7 -o $os_major_version -eq 8 ]; then
+  #   check_install_fastos_repo
+  #   rpm -q fuse >/dev/null && yum remove fuse -y
+  #   yum install FastCFS-auth-server fastDIR-server faststore-server FastCFS-fused -y
+  # else
+  #   echo "Unsupport OS: $uname" 1>&2
+  #   echo "Command setup and install can only be used for CentOS 7 or 8."
+  #   exit 1
+  # fi
+}
+
+check_module_param $*
+check_node_param $*
+load_cluster_groups
+echo "$fdir_group"
 
 case "$shell_command" in
   'setup')
-    install_dependent_libs
-    deploy_config_files
-    restart_servers
+    install_dependent_libs $*
+    deploy_config_files $*
+    restart_servers $*
   ;;
   'install')
-    install_dependent_libs
+    install_dependent_libs $*
   ;;
   'config')
-    deploy_config_files
+    deploy_config_files $*
   ;;
   'start')
     # Start services.
-    service_op start
+    service_op start $*
   ;;
   'restart')
     # Restart services.
-    service_op restart
+    service_op restart $*
   ;;
   'stop')
     # Start services.
-    service_op stop
+    service_op stop $*
   ;;
   'tail')
     # Start services.
