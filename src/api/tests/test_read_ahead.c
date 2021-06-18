cf98c1794460334b721f3a74144d8088e7cf359d
YuQing <384681@qq.com>
2021-06-18 12:11:09 +0800

    tests/test_read_ahead.c OK

--- src/api/tests/test_read_ahead.c
+++ src/api/tests/test_read_ahead.c
@@ -33,8 +33,11 @@ typedef struct {
 
 static char *ns = "test";
 static int64_t file_size;
-static int write_buffer_size;
-static int read_buffer_size;
+static int64_t write_buffer_size;
+static int64_t read_buffer_size;
+static int read_thread_count;
+static volatile int running_read_threads;
+static volatile int can_read = 0;
 
 static BlockArray barray;
 const char *filename = "/test.dat";
@@ -43,8 +46,9 @@ static void usage(char *argv[])
 {
     fprintf(stderr, "Usage: %s [-c config_filename=%s] "
             "-n [namespace=test]\n\t[-s file_size=64MB] "
-            "[-r read_buffer_size=4KB] [-w write_buffer_size=4KB]\n\n",
-            argv[0], FCFS_FUSE_DEFAULT_CONFIG_FILENAME);
+            "[-r read_buffer_size=4KB] [-w write_buffer_size=4KB]\n"
+            "\t[-R read_thread_count=1]\n\n", argv[0],
+            FCFS_FUSE_DEFAULT_CONFIG_FILENAME);
 }
 
 static int check_create_root_path()
@@ -111,148 +115,231 @@ static int init()
     return 0;
 }
 
-/*
+static void fill_buffer(char *buff, const int length,
+        const int64_t offset)
+{
+    int64_t block_index;
+    int count;
+    int i;
+    char *block;
+    char *p;
+
+    block_index = offset / BLOCK_SIZE;
+    count = length / BLOCK_SIZE;
+    p = buff;
+    for (i=0; i<count; i++) {
+        block = barray.blocks[(block_index + i) % barray.count];
+        memcpy(p, block, BLOCK_SIZE);
+        p += BLOCK_SIZE;
+    }
+}
+
 static void *write_thread(void *arg)
 {
     FCFSAPIFileContext fctx;
     FCFSAPIFileInfo fi;
     char *out_buff;
+    long thread_index;
+    int length;
     int write_bytes;
+    int result;
+    int64_t remain;
 
-    fctx.tid = getpid();
+    thread_index = (long)arg;
+    fctx.tid = getpid() + thread_index;
     fctx.omp.mode = 0755;
     fctx.omp.uid = geteuid();
     fctx.omp.gid = getegid();
-
     if ((result=fcfs_api_open(&fi, filename, O_CREAT |
                     O_WRONLY | O_TRUNC, &fctx)) != 0)
     {
-        return result;
+        __sync_bool_compare_and_swap(&can_read, 0, -1);
+        return NULL;
     }
-    if (offset == 0) {
-        result = fcfs_api_write(&fi, out_buff, length, &write_bytes);
-    } else {
-        result = fcfs_api_pwrite(&fi, out_buff, length, offset, &write_bytes);
-    }
-    if (result != 0) {
-        logError("file: "__FILE__", line: %d, "
-                "write to file %s fail, offset: %"PRId64", length: %d, "
-                "write_bytes: %d, errno: %d, error info: %s",
-                __LINE__, filename, offset, length, write_bytes,
-                result, STRERROR(result));
-        return result;
-    }
-    fcfs_api_close(&fi);
 
+    __sync_bool_compare_and_swap(&can_read, 0, 1);
+
+    out_buff = (char *)fc_malloc(write_buffer_size);
+    if (out_buff == NULL) {
+        fcfs_api_close(&fi);
+        return NULL;
+    }
+
+    remain = file_size;
+    while (remain > 0) {
+        fill_buffer(out_buff, write_buffer_size, file_size - remain);
+        length = FC_MIN(remain, write_buffer_size);
+        result = fcfs_api_write(&fi, out_buff, length, &write_bytes);
+        if (result != 0) {
+            logError("file: "__FILE__", line: %d, "
+                    "write to file %s fail, offset: %"PRId64", length: %d, "
+                    "write_bytes: %d, errno: %d, error info: %s",
+                    __LINE__, filename, file_size - remain, length,
+                    write_bytes, result, STRERROR(result));
+            break;
+        }
+
+        remain -= write_bytes;
+    }
+
+    fcfs_api_close(&fi);
+    free(out_buff);
     return NULL;
 }
-*/
 
-/*
-static void *read_thread(void *arg)
+static int read_test(const int thread_index)
 {
     FCFSAPIFileContext fctx;
     FCFSAPIFileInfo fi;
+    char *expect_buff;
     char *in_buff;
+    int wait_count;
+    int retry_count;
+    int length;
     int read_bytes;
+    int result;
+    int fail_count;
+    int64_t remain;
 
-    fctx.tid = getpid();
+    fctx.tid = getpid() + thread_index;
     fctx.omp.mode = 0755;
     fctx.omp.uid = geteuid();
     fctx.omp.gid = getegid();
-
     if ((result=fcfs_api_open(&fi, filename, O_RDONLY, &fctx)) != 0) {
         return result;
     }
-    in_buff = (char *)malloc(length);
+
+    in_buff = (char *)fc_malloc(read_buffer_size * 2);
     if (in_buff == NULL) {
-        logError("file: "__FILE__", line: %d, "
-                "malloc %d bytes fail", __LINE__, length);
+        fcfs_api_close(&fi);
         return ENOMEM;
     }
+    expect_buff = in_buff + read_buffer_size;
 
-    if ((result=fcfs_api_lseek(&fi, offset, SEEK_SET)) != 0) {
-        return result;
-    }
+    fail_count = 0;
+    wait_count = retry_count = 0;
+    remain = file_size;
+    while (remain > 0) {
+        length = FC_MIN(remain, read_buffer_size);
+        result = fcfs_api_pread(&fi, in_buff, length,
+                file_size - remain, &read_bytes);
+        if (result != 0) {
+            logError("file: "__FILE__", line: %d, thread index: %d, "
+                    "read from file %s fail, offset: %"PRId64", length: %d, "
+                    "read_bytes: %d, errno: %d, error info: %s", __LINE__,
+                    thread_index, filename, file_size - remain, length,
+                    read_bytes, result, STRERROR(result));
+            break;
+        }
 
-    if (offset == 0) {
-        result = fcfs_api_read(&fi, in_buff, length, &read_bytes);
-    } else {
-        //result = fcfs_api_pread(&fi, in_buff, length, offset, &read_bytes);
-        result = fcfs_api_read(&fi, in_buff, length, &read_bytes);
-    }
+        if (read_bytes != length) {
+            ++wait_count;
+            fc_sleep_ms(1);
+            continue;
+        }
 
-    if (result != 0) {
-        logError("file: "__FILE__", line: %d, "
-                "read from file %s fail, offset: %"PRId64", length: %d, "
-                "read_bytes: %d, errno: %d, error info: %s",
-                __LINE__, filename, offset, length, read_bytes,
-                result, STRERROR(result));
-        return result;
-    }
+        fill_buffer(expect_buff, read_buffer_size, file_size - remain);
+        result = memcmp(in_buff, expect_buff, length);
+        if (result != 0) {
+            if (fail_count++ < 10) {
+                fc_sleep_ms(1);
+                ++retry_count;
+                continue;
+            }
 
-    if (read_bytes != length) {
-        logError("file: "__FILE__", line: %d, "
-                "read bytes: %d != slice length: %d",
-                __LINE__, read_bytes, length);
-        return EINVAL;
-    }
+            logError("file: "__FILE__", line: %d, thread index: %d, "
+                    "file offset: %"PRId64", read and expect buffer "
+                    "compare result: %d != 0", __LINE__, thread_index,
+                    file_size - remain, result);
+            break;
+        }
 
-    result = memcmp(in_buff, out_buff, length);
-    if (result != 0) {
-        printf("read and write buffer compare result: %d != 0\n", result);
-        return EINVAL;
+        if (fail_count > 0) {
+            fail_count = 0;
+        }
+        remain -= read_bytes;
     }
 
     fcfs_api_close(&fi);
+    free(in_buff);
+
+    if (wait_count > 0 || retry_count > 0) {
+        logInfo("file: "__FILE__", line: %d, "
+                "thread index: %d, wait file content ready count: %d, "
+                "retry count: %d", __LINE__, thread_index, wait_count,
+                retry_count);
+    }
+
+    return result;
 }
-*/
+
+static void *read_thread(void *arg)
+{
+    int thread_index;
+
+    thread_index = (long)arg;
+    if (read_test(thread_index) == 0) {
+        logInfo("file: "__FILE__", line: %d,  thread index: %d, "
+                "read test successfully!", __LINE__, thread_index);
+    }
+
+    FC_ATOMIC_DEC(read_thread_count);
+    return NULL;
+}
 
 int main(int argc, char *argv[])
 {
     const bool publish = false;
     const char *config_filename = FCFS_FUSE_DEFAULT_CONFIG_FILENAME;
 	int ch;
+    int i;
 	int result;
-    char *endptr;
+    int status;
+    long thread_index;
+    pthread_t wtid;
+    pthread_t rtid;
 
+    read_thread_count = 1;
     file_size = 64 * 1024 * 1024;
     write_buffer_size = read_buffer_size = 4 * 1024;
-    while ((ch=getopt(argc, argv, "hc:o:n:i:l:S:AT")) != -1) {
+    while ((ch=getopt(argc, argv, "hc:n:s:w:r:R:")) != -1) {
         switch (ch) {
             case 'h':
                 usage(argv);
-                break;
+                return 0;
             case 'c':
                 config_filename = optarg;
                 break;
             case 'n':
                 ns = optarg;
                 break;
-                break;
             case 's':
-                file_size = strtol(optarg, &endptr, 10);
+                if ((result=parse_bytes(optarg, 1, &file_size)) != 0) {
+                    return EINVAL;
+                }
                 if (file_size < 1024 * 1024) {
                     fprintf(stderr, "file size: %"PRId64" "
                             "is too small < 1MB", file_size);
                     return EINVAL;
                 }
+                file_size = MEM_ALIGN_CEIL(file_size, 1024 * 1024);
                 break;
             case 'w':
-                write_buffer_size = strtol(optarg, &endptr, 10);
-                if (write_buffer_size <= 0) {
-                    fprintf(stderr, "invalid write buffer size: %d <= 0",
-                            write_buffer_size);
+                if ((result=parse_bytes(optarg, 1, &write_buffer_size)) != 0) {
                     return EINVAL;
                 }
+                write_buffer_size = MEM_ALIGN_CEIL(
+                        write_buffer_size, 1024);
                 break;
             case 'r':
-                read_buffer_size = strtol(optarg, &endptr, 10);
-                if (read_buffer_size <= 0) {
-                    fprintf(stderr, "invalid read buffer size: %d <= 0",
-                            read_buffer_size);
+                if ((result=parse_bytes(optarg, 1, &read_buffer_size)) != 0) {
                     return EINVAL;
                 }
+                read_buffer_size = MEM_ALIGN_CEIL(
+                        read_buffer_size, 1024);
+                break;
+            case 'R':
+                read_thread_count = strtol(optarg, NULL, 10);
                 break;
             default:
                 usage(argv);
@@ -279,6 +366,35 @@ int main(int argc, char *argv[])
 
     if ((result=fcfs_api_start()) != 0) {
         return result;
+    }
+
+    thread_index = 1;
+    if ((result=pthread_create(&wtid, NULL, write_thread,
+                    (void *)(thread_index++))) != 0)
+    {
+        return result;
+    }
+
+    while ((status=FC_ATOMIC_GET(can_read)) == 0) {
+        fc_sleep_ms(10);
+    }
+
+    if (status < 0) {
+        return EIO;
+    }
+
+    running_read_threads = read_thread_count;
+    for (i=0; i<read_thread_count; i++) {
+        if ((result=pthread_create(&rtid, NULL, read_thread,
+                        (void *)(thread_index++))) != 0)
+        {
+            return result;
+        }
+    }
+    pthread_join(wtid, NULL);
+
+    while (FC_ATOMIC_GET(read_thread_count) != 0) {
+        fc_sleep_ms(10);
     }
 
     fcfs_api_terminate();
