7ae048ef848d11fba3b0c6072aa7008371013a7a
YuQing <384681@qq.com>
2023-01-12 09:52:00 +0800

    add files: getgroups.[hc]

--- src/fuse/fuse_wrapper.c
+++ src/fuse/fuse_wrapper.c
@@ -21,10 +21,9 @@
 #include <unistd.h>
 #include <pwd.h>
 #include <grp.h>
-#include "fastcommon/common_define.h"
-#include "fastcommon/sched_thread.h"
-#include "fastcommon/system_info.h"
+#include "fastcommon/logger.h"
 #include "global.h"
+#include "getgroups.h"
 #include "fuse_wrapper.h"
 
 #define FS_READDIR_BUFFER_INIT_NONE        0
@@ -34,106 +33,10 @@
 struct fuse_conn_info_opts *g_fuse_cinfo_opts;
 static struct fast_mblock_man fh_allocator;
 
-static inline int get_last_id(const char *buff,
-        const char *tag_str, const int tag_len)
-{
-    char *start;
-    char *last;
-
-    if ((start=strstr(buff, tag_str)) == NULL) {
-        return -1;
-    }
-
-    start += tag_len;
-    if ((last=strchr(start, '\n')) == NULL) {
-        return -1;
-    }
-
-    do {
-        --last;
-    } while (last > start && (*last >= '0' && *last <= '9'));
-
-    return strtol(last, NULL, 10);
-}
-
-static int fuse_get_groups(const pid_t pid, const uid_t fsuid,
-        const gid_t fsgid, const int size, gid_t *list)
-{
-#define UID_TAG_STR   "\nUid:"
-#define UID_TAG_LEN   (sizeof(UID_TAG_STR) - 1)
-#define GID_TAG_STR   "\nGid:"
-#define GID_TAG_LEN   (sizeof(GID_TAG_STR) - 1)
-#define GROUPS_TAG_STR   "\nGroups:"
-#define GROUPS_TAG_LEN   (sizeof(GROUPS_TAG_STR) - 1)
-
-    char filename[64];
-    char buff[1024];
-    struct passwd *user;
-    char *p;
-    char *end;
-    int fd;
-    int len;
-    uid_t euid;
-    gid_t egid;
-    gid_t val;
-    int count;
-
-    sprintf(filename, "/proc/%d/status", pid);
-    fd = open(filename, O_RDONLY);
-    if (fd < 0) {
-        return 0;
-    }
-
-    len = read(fd, buff, sizeof(buff));
-    close(fd);
-    if (len <= 0) {
-        return 0;
-    }
-
-    buff[len - 1] = '\0';
-    euid = get_last_id(buff, UID_TAG_STR, UID_TAG_LEN);
-    egid = get_last_id(buff, GID_TAG_STR, GID_TAG_LEN);
-    if (fsuid == euid && fsgid == egid) {
-        if ((p=strstr(buff, GROUPS_TAG_STR)) == NULL) {
-            return 0;
-        }
-
-        p += GROUPS_TAG_LEN;
-        count = 0;
-        while (count < size) {
-            val = strtoul(p, &end, 10);
-            if (end == p) {
-                break;
-            }
-
-            if (val != fsgid) {
-                list[count++] = val;
-            }
-            p = end;
-        }
-    } else {
-        logInfo("fsuid: %d, euid: %d, fsgid: %d, egid: %d", fsuid, euid, fsgid, egid);
-        if ((user=getpwuid(fsuid)) == NULL) {
-            return 0;
-        }
-
-        count = size;
-        if (getgrouplist(user->pw_name, fsgid, list, &count) < 0) {
-            return 0;
-        }
-    }
-
-    return count;
-}
-
 static inline void set_operator_by_req(const struct fuse_ctx *fctx,
         FDIRDentryOperator *oper, char *buff)
 {
     int count;
-    gid_t list[FDIR_MAX_USER_GROUP_COUNT];
-    const gid_t *group;
-    const gid_t *end;
-    char *p;
 
     if (g_fcfs_api_ctx.owner.type == fcfs_api_owner_type_fixed) {
         *oper = g_fcfs_api_ctx.owner.oper;
@@ -145,25 +48,7 @@ static inline void set_operator_by_req(const struct fuse_ctx *fctx,
         return;
     }
 
-    count = fuse_get_groups(fctx->pid, fctx->uid, fctx->gid,
-            FDIR_MAX_USER_GROUP_COUNT, list);
-    if (count <= 0) {
-        count = 0;
-    } else if (count == 1 && list[0] == fctx->gid) {
-        count = 0;
-    } else {
-        end = list + count;
-        for (group=list, p=buff; group<end; group++) {
-            if (*group != fctx->gid) {
-                int2buff(*group, p);
-                p += 4;
-
-            }
-            //logInfo("%d. gid: %d", (int)(group - list) + 1, *group);
-        }
-        count = (p - buff) / 4;
-    }
-    //logInfo("line: %d, count: %d, first gid: %d", __LINE__, count, count > 0 ? buff2int(buff) : -1);
+    count = fcfs_get_groups(fctx->pid, fctx->uid, fctx->gid, buff);
     FDIR_SET_OPERATOR(*oper, fctx->uid, fctx->gid, count, buff);
 }
 
@@ -591,10 +476,6 @@ static int do_open(fuse_req_t req, FDIRDEntryInfo *dentry,
     }
 
     if ((result=fcfs_api_open_by_dentry(fh, dentry, fi->flags, fctx)) != 0) {
-        logError("file: "__FILE__", line: %d, func: %s, "
-            "ino: %"PRId64", fh: %"PRId64", flags: %d, result: %d\n",
-            __LINE__, __FUNCTION__, dentry->inode, fi->fh, fi->flags, result);
-
         fast_mblock_free_object(&fh_allocator, fh);
         if (!(result == EISDIR || result == ENOENT)) {
             result = EACCES;
