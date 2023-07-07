a61f5686624a82603090c6a9c2c8561a51cec4ba
YuQing <384681@qq.com>
2023-07-07 08:36:33 +0800

    upgrade version to 4.2.0

--- src/common/fcfs_global.c
+++ src/common/fcfs_global.c
@@ -16,5 +16,5 @@
 #include "fcfs_global.h"
 
 FCFSGlobalVars g_fcfs_global_vars = {
-    {4, 1, 0}
+    {4, 2, 0}
 };
--- src/vote/common/vote_global.c
+++ src/vote/common/vote_global.c
@@ -16,5 +16,5 @@
 #include "vote_global.h"
 
 FCFSVoteGlobalVars g_fcfs_vote_global_vars = {
-    {4, 1, 0}
+    {4, 2, 0}
 };
