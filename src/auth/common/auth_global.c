943da77e77f8bce7814ef223649e48cae0ee9b7d
YuQing <384681@qq.com>
2024-01-30 11:40:56 +0800

    upgrade version to 5.2.0

--- src/auth/common/auth_global.c
+++ src/auth/common/auth_global.c
@@ -16,5 +16,5 @@
 #include "auth_global.h"
 
 FCFSAuthGlobalVars g_fcfs_auth_global_vars = {
-    {5, 1, 0}
+    {5, 2, 0}
 };
--- src/vote/common/vote_global.c
+++ src/vote/common/vote_global.c
@@ -16,5 +16,5 @@
 #include "vote_global.h"
 
 FCFSVoteGlobalVars g_fcfs_vote_global_vars = {
-    {5, 1, 0}
+    {5, 2, 0}
 };
